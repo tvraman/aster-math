db0baf16454d378264bdfe286c0da46fdaee6478
raman <tv.raman.tv@gmail.com>
2015-04-28 08:54:08 -0700

    eliminated most global/free var usage. Need to find clisp equivalent of process-status etc.

--- lisp-code/emacspeak/tts.lisp
+++ lisp-code/emacspeak/tts.lisp
@@ -1,19 +1,7 @@
 ;;; tts.lisp -- Common Lisp interface  to Emacspeak speech servers
-;;; $Id: tts.lisp 7078 2011-06-29 22:07:46Z tv.raman.tv $
 ;;; $Author: tv.raman.tv $
 ;;; Description:   Interface Common Lisp   to Emacspeak TTS servers
 ;;; Keywords: AsTeR, Emacspeak, Audio Desktop
-;;; {  LCD Archive entry:
-
-;;; LCD Archive Entry:
-;;; emacspeak| T. V. Raman |raman@cs.cornell.edu
-;;; A speech interface to Emacs |
-;;; $Date: 2011-06-29 15:07:46 -0700 (Wed, 29 Jun 2011) $ |
-;;;  $Revision: 7078 $ |
-;;; Location undetermined
-;;;
-
-;;; }
 ;;; {  Copyright:
 
 ;;; Copyright (C)  2011, T. V. Raman<raman@cs.cornell.edu>
@@ -51,91 +39,102 @@
 (defvar *tts-process* nil
   "Handle to tts server connection.")
 
-(defvar *tts-dtk*
-  (concatenate 'string   *emacspeak* "servers/dtk-exp")
-  "DTK tcl server")
+(defvar *tts-engine* (tts-32-outloud)
+  "Default TTS engine to use.")
+(proclaim '(inline tts-process))
+(defun tts-process ()
+  "Return handle to TTS process."
+  (declare (special *tts-process*))
+  *tts-engine)
+(defun tts-engine ()
+  "Return default tts-engine."
+  (declare (special *tts-engine*))
+  *tts-engine)
 
-(defvar *tts-outloud*
-  (concatenate 'string   *emacspeak* "servers/outloud")
-  "Outloud tcl server")
 
-(defvar *tts-32-outloud*
-  (concatenate 'string   *emacspeak* "servers/32-outloud")
-  "Outloud tcl server")
 
-(defvar *tts-engine* *tts-32-outloud*
-  "Default TTS  engine. User settable.")
+(proclaim '(inline tts-process))
+(defun tts-process ()
+  "Return handle to TTS process."
+  (declare (special *tts-process*))
+  *tts-process)
+
+(defun dtk-exp ()
+  "Return name of dtk-exp server."
+  (declare (special *emacspeak*))
+  (concatenate 'string   *emacspeak* "servers/dtk-exp"))
+
+(defun tts-outloud ()
+  "Outloud tcl server"
+  (declare (special *emacspeak*))
+  (concatenate 'string   *emacspeak* "servers/outloud"))
+
+(defun tts-32-outloud ()
+  "Return location of 32-outloud server."
+  (declare (special *emacspeak*))
+  (concatenate 'string   *emacspeak* "servers/32-outloud"))
 
 ;;; }
 ;;; {Internal  Functions
 
 (defun tts-open ()
   "Open a TTS session."
-  (setq *tts-process*
+  (setf(tts-process)
         (ext:run-program
-         *tts-engine* nil :wait nil  :input :stream)))
+         (tts-engine) nil :wait nil  :input :stream)))
 
 (defun tts-close ()
   "Close a TTS session."
-  (when(and  (ext:process-p *tts-process*)
-             (ext:process-alive-p *tts-process*))
-    (ext:process-close *tts-process*))
-  (setq *tts-process* nil))
+  (let ((p (tts-process)))
+  (when(and  (ext:process-p p)
+             (ext:process-alive-p p))
+    (ext:process-close p))
+  (setf (tts-process) nil))
 
 (defun tts-running-p ()
   "Is there a tts process up and running?"
-  (and *tts-process*
-       (ext:process-p *tts-process*)
-       (ext:process-alive-p *tts-process*)))
+  (let ((p (tts-process)))
+  (and  p
+       (ext:process-p p)
+       (ext:process-alive-p p)))
 
-(defvar *tts-stop-immediately* t
-  "Stop speech immediately.")
+
 (defun tts-queue (text)
   "Queue text to speak."
-  (unless (and  *tts-process*
-                (ext:process-alive-p *tts-process*))
-    (tts-open))
-  (let ((i (ext:process-input *tts-process*)))
-    (write-line (format nil "q {~s}" text) i)
-    (force-output i)))
+  (let ((p (tts-process)))
+    (unless (and  p (ext:process-alive-p p))
+      (setq p (tts-open)))
+    (let ((i (ext:process-input p)))
+      (write-line (format nil "q {~s}" text) i)
+      (force-output i))))
 
 (defun tts-force ()
   "Speak all queued text."
-  (let ((i (ext:process-input *tts-process*)))
+  (let ((i (ext:process-input (tts-process))))
     (write-line "d" i)
     (force-output i)))
 
 ;;; }
 ;;; {Exported Functions
+
 (defun tts-stop ()
   "Stop speech."
-  (let ((i (ext:process-input *tts-process*)))
+  (let ((i (ext:process-input (tts-process))))
       (write-line "s"  i)
       (force-output i)))
 
 (defun tts-speak (text)
-  "Say some text."
-  (unless (and  *tts-process*
-                (ext:process-alive-p *tts-process*))
+  "Speak text."
+  (unless (and  (tts-process)
+                (ext:process-alive-p (tts-process)))
     (tts-open))
-  (let ((i (ext:process-input *tts-process*)))
-    (when *tts-stop-immediately*
+  (let ((i (ext:process-input (tts-process))))
       (write-line "s"  i)
-      (force-output i))
-    (write-line (format nil "q ~s\;d" text) i)
+      (force-output i)
+    (write-line (format nil "q ~s\nd" text) i)
     (force-output i)))
 
-(defun tts-say (text)
-  "Say some text."
-  (unless (and  *tts-process*
-                (ext:process-alive-p *tts-process*))
-    (tts-open))
-  (let ((i (ext:process-input *tts-process*)))
-    (when *tts-stop-immediately*
-      (write-line "s"  i)
-      (force-output i))
-    (write-line (format nil "tts_say  ~s" text) i)
-    (force-output i)))
+
 
 (defun tts-speak-list (lines)
   "Speak an arbitrary number of lines."
@@ -145,10 +144,10 @@
 
 (defun tts-letter (text)
   "Speak letter."
-  (unless (and  *tts-process*
-                (ext:process-alive-p *tts-process*))
+  (unless (and  (tts-process)
+                (ext:process-alive-p (tts-process)))
     (tts-open))
-  (let ((i (ext:process-input *tts-process*)))
+  (let ((i (ext:process-input (tts-process))))
     (write-line (format nil "l ~s" text) i)
     (force-output i)))
 
