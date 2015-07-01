54a870f7eee6b6bf5dddc0fbfd99710d7a49b7f8
raman <tv.raman.tv@gmail.com>
2015-07-01 10:48:00 -0700

    format: use ~a not ~s for when writing out strings

--- lisp-code/tts/tts.lisp
+++ lisp-code/tts/tts.lisp
@@ -105,20 +105,20 @@
   "Queue TTS code  to engine."
   (let ((i (tts-input (tts))))
     (unless i (setq i (tts-open)))
-    (format i "c {~s}~%" cmd) 
+    (format i "c {~a}~%" cmd) 
     (finish-output i)))
 
 (defun queue (text)
   "Queue text to speak."
   (let ((i (tts-input (tts))))
     (unless i (setq i (tts-open)))
-    (format i "q {~s}~%" text) 
+    (format i "q {~a}~%" text) 
     (finish-output i)))
 
 (defun pause (ms)
   "Send silence"
 (let ((i (tts-input (tts))))
-    (format i "sh {~s}~%" ms) 
+    (format i "sh {~a}~%" ms) 
     (finish-output i))  )
 
 (defun force ()
@@ -137,7 +137,7 @@
   "Speak text."
   (unless (tts-input (tts)) (tts-open))
   (let ((i (tts-input (tts))))
-    (format i "q {~s}~%" text)
+    (format i "q {~a}~%" text)
     (format i "d~%")
     (finish-output i)))
 
@@ -151,7 +151,7 @@
   "Speak letter."
   (unless (tts-input (tts)) (tts-open))
   (let ((i (tts-input (tts))))
-    (format i "l ~s~%" text)
+    (format i "l ~a~%" text)
     (finish-output i)))
 
 ;;; }
