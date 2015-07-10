a5bf5559b6cc7554a90607c12dc7a0423b695cea
raman <tv.raman.tv@gmail.com>
2015-07-10 14:57:23 -0700

    added acss

---
+++ lisp-code/afl/acss-speech.lisp
@@ -0,0 +1,24 @@
+;;;   -*- Syntax: Common-Lisp; Package: afl; Base: 10; Mode: LISP -*-    ;;;
+;;;                                                                       ;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;; Copyright (C) 1990, 1991, 1992, 1993, 1994 by T. V. Raman
+;;; All Rights Reserved
+;;;
+(in-package :afl)
+(proclaim '(optimize (compilation-speed 0) (safety 1) (speed 3)))
+
+;;; export all dimension names.
+;;; This defines the Speech-Space in terms of Aural CSS (ACSS).
+;;; { *list-of-speech-dimensions*
+
+;;; Dimensions are from Aural CSS (ACSS).
+(defstruct acss
+  "Aural CSS"
+  family gain
+  left-volume right-volume
+  average-pitch pitch-range
+  stress richness
+  punctuations
+  ) 
+;;; }
