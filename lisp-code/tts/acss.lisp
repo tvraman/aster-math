a7f4e11d3d2e7d9ee7ea5c54c71fabf33a9de766
T. V. Raman <tv.raman.tv@gmail.com>
2016-01-06 09:53:59 -0800

    cleanup

--- lisp-code/tts/acss.lisp
+++ lisp-code/tts/acss.lisp
@@ -1,16 +1,32 @@
-;;;   -*- Syntax: Common-Lisp; Package: afl; Base: 10; Mode: LISP -*-    ;;;
-;;;                                                                       ;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;   -*- Syntax: Common-Lisp; Package: tts;  Mode: LISP -*-    ;;;
+;;; acss.lisp -- Common Lisp Implementation of Aural CSS
+;;; $Author: tv.raman.tv $
+;;; Description: Aural CSS
+;;; Keywords: AsTeR, Emacspeak, Audio Desktop
+;;; { Copyright:
 
-;;; Copyright (C) 1990, 1991, 1992, 1993, 1994 by T. V. Raman
-;;; All Rights Reserved
+;;; Copyright (C) 2011 -- 2016, T. V. Raman<tv.raman.tv@gmail.com>
+;;; All Rights Reserved.
 ;;;
+;;; This file is not part of GNU Emacs, but the same permissions apply.
+;;;
+;;; GNU Emacs is free software; you can redistribute it and/or modify
+;;; it under the terms of the GNU General Public License as published by
+;;; the Free Software Foundation; either version 2, or (at your option)
+;;; any later version.
+;;;
+;;; GNU Emacs is distributed in the hope that it will be useful,
+;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+;;; GNU General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU General Public License
+;;; along with GNU Emacs; see the file COPYING. If not, write to
+;;; the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+
+;;; }m
 (in-package :afl)
 (proclaim '(optimize (compilation-speed 0) (safety 1) (speed 3)))
-
-;;; export all dimension names.
-;;; This defines the Speech-Space in terms of Aural CSS (ACSS).
-;;; Dimensions are from Aural CSS (ACSS).
 
 (defstruct acss
   "Aural CSS"
@@ -20,4 +36,4 @@
   stress richness
   punctuations
   ) 
-;;; }
+
