a16c71e4bec8987db02ec69ce93add9be7211b9d
T.V Raman <tv.raman.tv@gmail.com>
2022-10-29 09:13:20 -0700

    set-state

--- lisp-code/afl/06-synthesizer-codes.lisp
+++ lisp-code/afl/06-synthesizer-codes.lisp
@@ -2,66 +2,64 @@
 ;;;                                                                       ;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
-;;; Copyright (C) 1990, 1991, 1992, 1993, 1994by T. V. Raman 
+;;; Copyright (C) 1990, 1991, 1992, 1993, 1994by T. V. Raman
 ;;; All Rights Reserved
 ;;;
 (in-package :afl)
 (proclaim '(optimize (compilation-speed 0) (safety 1) (speed 3)))
 
-
-
 ;;; Initially assumes dectalk, but to be flexible enough to allow any
 ;;; synthesizer.
 
-;;{{{ Start and end markers 
+;;{{{ Start and end markers
 
 ;;; Variable: *BEGIN-COMMAND*                                Author: raman
 ;;; Created: Tue Aug 11 13:40:16 1992
-;;; external variable: 
+;;; external variable:
 (defvar *begin-command* "[" " string that begins a synthesizer command")
 
 ;;; Variable: *END-COMMAND*                                  Author: raman
 ;;; Created: Tue Aug 11 13:40:44 1992
-;;; external variable: 
+;;; external variable:
 (defvar *end-command* "]" "string that ends a synthesizer command")
 
 ;;}}}
 ;;{{{ generate-synthesizer-command
+
 ;;; Modified: Thu Aug 20 10:40:53 EDT 1992
 ;;; Modified to return string rather than call format each time.
 ;;; Function: GENERATE-SYNTHESIZER-COMMAND                   Author: raman
 ;;; Created: Tue Aug 11 13:41:18 1992
 
-(defun generate-synthesizer-command (dimension value) 
+(defun generate-synthesizer-command (dimension value)
   "Generates synthesizer command that sets dimension dimension to
   value value"
   (flet (
          (bound-in-interval(dim-name   value)
            "fix value inside permitted interval"
-;           (format t "~& dim-name = ~a
-;~& value = ~a"
-;                   dim-name value)
+                                        ;           (format t "~& dim-name = ~a
+                                        ;~& value = ~a"
+                                        ;                   dim-name value)
            (min
             (max (minimum-value dim-name)
                  value)
             (maximum-value dim-name ))))
     (when (equal dimension 'voice)
       (setf value (get-voice-code value)))
-    (concatenate 'string
-                 *begin-command*
-                 " "
-                 (get-synthesizer-code dimension)
-                 (if (stringp value)
-                     value
-                     (prin1-to-string
-                      (bound-in-interval dimension 
-                                         (round value))))
-                 " "
-                 *end-command*))
-  )
+    (concatenate
+     'string
+     *begin-command*
+     " "
+     (get-synthesizer-code dimension)
+     (if (stringp value)
+         value
+         (prin1-to-string
+          (bound-in-interval dimension
+                             (round value))))
+     " "
+     *end-command*)))
 
 ;;}}}
-
 ;;{{{ *voice-codes-table*
 
 ;;; Variable: *VOICE-CODES-TABLE*                            Author: raman
@@ -73,15 +71,15 @@
 ;;; Created: Tue Aug 11 14:58:10 1992
 
 (defstruct (voice-code
-             ;(:named)
-             (:type list))
+                                        ;(:named)
+            (:type list))
   voice
   code)
 ;;; Modified: Thu Aug 20 09:37:22 EDT 1992
 ;;; export voice whose code is defined
 ;;; Function:  DEFINE-VOICE-CODE Author: raman Created: Tue Aug 11 14:56:56 1992
 
-(defun define-voice-code (voice code) 
+(defun define-voice-code (voice code)
   "define code as code for voice voice"
   (export (list voice))
   (push
@@ -97,18 +95,18 @@
 (define-voice-code 'frank "f")
 (define-voice-code 'betty "b")
 (define-voice-code 'wendy "w")
-(define-voice-code  'rita  "r") 
+(define-voice-code  'rita  "r")
 (define-voice-code 'ursula "u")
 (define-voice-code 'kid "k")
 
 ;;; Function: GET-VOICE-CODE                                 Author: raman
 ;;; Created: Tue Aug 11 15:03:42 1992
 
-(defun get-voice-code (voice) 
+(defun get-voice-code (voice)
   "get code for voice"
   (let
       ((voice-code
-        (find voice *voice-codes-table* :key #'voice-code-voice )))
+         (find voice *voice-codes-table* :key #'voice-code-voice )))
     (if  voice-code
          (voice-code-code voice-code)
          (error "Voice code for voice ~a not defined"
@@ -118,4 +116,3 @@
   )
 
 ;;}}}
-
