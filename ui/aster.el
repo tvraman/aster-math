3b477421b338d3c3f5411d12609caee2cfc675a6
T.V Raman <tv.raman.tv@gmail.com>
2022-11-02 11:36:15 -0700

    rename

--- ui/aster.el
+++ ui/aster.el
@@ -50,7 +50,7 @@
   "Directory where common-lisp files are stored.")
 
 (defvar aster-setup
-  (expand-file-name "aster-setup.lisp" aster-root)
+  (expand-file-name "setup.lisp" aster-root)
   "Common Lisp file loaded to configure Aster.")
 
 ;;}}}
