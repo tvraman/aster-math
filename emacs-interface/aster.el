fe69f07a5e0b8d05e08bcf467c3adff99ade8411
T.V Raman <tv.raman.tv@gmail.com>
2022-11-01 20:53:52 -0700

    backup

--- emacs-interface/aster.el
+++ emacs-interface/aster.el
@@ -59,11 +59,15 @@
   "Load and configure AsTeR."
   (interactive)
   (cl-declare (special aster-setup))
-  (let ((afl-init "(afl:tts-init)")
-        (welcome "(afl:tts-speak \"Welcome to Aster\") "))
-    (slime-load-file aster-setup)
-    (slime-eval-save afl-init)
-    (slime-eval-save welcome)))
+    (if-let
+        ((welcome "(afl:tts-speak \"Welcome to Aster\") ")
+         (afl-init "(afl:tts-init)")
+         (_live (buffer-live-p (get-buffer "*inferior-lisp*"))))
+        (progn
+          (or (slime-connection) (slime))
+          (slime-load-file aster-setup)
+          (slime-eval-save afl-init)
+          (slime-eval-save welcome))))
 
 (defun aster-cmd (string)
   "Read Aster sexp from minibuffer and run it."
