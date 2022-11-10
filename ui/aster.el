1c51c5d66d36fa048837575fa03987d4d3db2d05
T.V Raman <tv.raman.tv@gmail.com>
2022-11-10 07:20:08 -0800

    cleanup

--- ui/aster.el
+++ ui/aster.el
@@ -20,7 +20,7 @@
 ;;;
 ;;; You should have received a copy of the GNU General Public License
 ;;; along with GNU Emacs; see the file COPYING.  If not, write to
-;;; the Free Software Foundation, 51 Franklin Street, Fifth Floor, Boston,MA 02110-1301, USA.
+;; the Free Software Foundation, 51 Franklin Street, Fifth Floor,
 
 ;;}}}
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -61,7 +61,9 @@
   (string-trim
    (shell-command-to-string
     (format
-     "pacmd list-sink-inputs | grep -i -B 15 %s | grep index | cut -d ':' -f 2"
+     (concat
+      "pacmd list-sink-inputs | grep -i -B 15 %s |"
+      "grep index | cut -d ':' -f 2")
      pattern))))
 
 (defsubst aster-check ()
