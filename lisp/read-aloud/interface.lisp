9a1a96edcc1c1eb329b585e4d1650d29790550cd
T.V Raman <tv.raman.tv@gmail.com>
2022-11-19 07:43:42 -0800

    worked around package issues.

--- lisp/read-aloud/interface.lisp
+++ lisp/read-aloud/interface.lisp
@@ -9,27 +9,30 @@
 (proclaim '(optimize (compilation-speed 0) (safety 1) (speed 3)))
 (in-package :aster)
 
-(export '(aster-file aster-string))
-;;; Variable: *LEX-PROGRAM*                                  Author: raman
-;;; Created: Fri Feb 21 09:15:19 1992
+(export '(aster-file aster-text))
 
 ;;; Function: PARSE-ARTICLE                                 Author: raman
 ;;; Created: Fri Feb 21 09:10:37 1992
 
 (defun aster-file (filename)
   "Aster a  Latex article "
-  (let* ((process
+  (let* ((doc nil)
+         (process
            (run-program
             (namestring  (merge-pathnames "lexer/lispify" *lisp-dir*)) nil
             :input (open filename) :wait t :output  :stream))
          (output (process-output process)))
-    (read-aloud (create-article (read output)))))
+    (setq doc (create-article (read output)))
+    
+    (read-aloud doc)))
 
-(defun aster-string (latex-string) 
+(defun aster-text (latex-string) 
   "Aster  a Latex article passed as a string."
-  (let ((process
+  (let ((doc nil)
+        (process
           (sb-ext:run-program
            (namestring  (merge-pathnames "lexer/lispify" *lisp-dir*)) nil
            :input (make-string-input-stream latex-string)
            :wait t :output  :stream)))
-    (read-aloud (create-article (read (sb-ext:process-output process))))))
+    (setq doc (create-article (read (sb-ext:process-output process))))
+    (read-aloud doc)))
