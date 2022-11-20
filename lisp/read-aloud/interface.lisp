1f502743727ab56c41510a0de8343aa5fa21a6bd
T.V Raman <tv.raman.tv@gmail.com>
2022-11-20 11:39:12 -0800

    refactored for conciseness

--- lisp/read-aloud/interface.lisp
+++ lisp/read-aloud/interface.lisp
@@ -1,7 +1,4 @@
 ;;;   -*-   Mode: LISP -*-    ;;;
-;;;                                                                       ;;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
 ;;; Copyright (C) 1990, 1991, 1992, 1993, 1994by T. V. Raman 
 ;;; All Rights Reserved
 ;;;
@@ -11,22 +8,22 @@
 
 (export '(aster-file aster-text))
 
-;;; Function: PARSE-ARTICLE                                 Author: raman
-;;; Created: Fri Feb 21 09:10:37 1992
+(defvar *lexer*
+  (namestring  (merge-pathnames "lexer/lispify" *lisp-dir*))
+  "Lexical analyser.")
+
+(defun doc-from-stream (input)
+  "Return document parsed by reading  input stream."
+  (let ((process
+          (run-program *lexer* nil :input input  :wait t :output  :stream)))
+    (create-article (read (process-output process)))))
 
 (defun aster-file (filename)
   "Aster a  Latex article "
-  (let ((process
-          (run-program
-            (namestring  (merge-pathnames "lexer/lispify" *lisp-dir*)) nil
-            :input (open filename) :wait t :output  :stream)))
-    (read-aloud (create-article (read (process-output process))))))
+  (let ((doc (doc-from-stream (open filename))))
+    (read-aloud doc)))
 
-(defun aster-text (latex-string) 
+(defun aster-text (latex) 
   "Aster  a Latex article passed as a string."
-  (let ((process
-          (run-program
-           (namestring  (merge-pathnames "lexer/lispify" *lisp-dir*)) nil
-           :input (make-string-input-stream latex-string)
-           :wait t :output  :stream)))
-    (read-aloud (create-article (read (process-output process))))))
+  (let ((doc (doc-from-stream (make-string-input-stream latex))))
+    (read-aloud doc)))
