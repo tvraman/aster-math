537e9223e8c37cdec52ea0fae19eb9e352d346ba
T.V Raman <tv.raman.tv@gmail.com>
2022-11-26 13:48:05 -0800

    cleanup

--- lisp/aster.lisp
+++ lisp/aster.lisp
@@ -14,18 +14,16 @@
   "Load AsTeR modules and initialize system."
   (when (uiop:getenv "ASTER_TTS")
     (setf (uiop:getenv "PULSE_SINK") (uiop:getenv "ASTER_TTS")))
-  (let (                         ; (*muffled-warnings* 'style-warning)
-        (*print-case* :downcase))
-    (mapc #'asdf:load-system
-          '(:parser :afl  :read-aloud :browse))
+  (let ((*print-case* :downcase))
+    (mapc #'asdf:load-system '(:parser :afl  :read-aloud :browse))
     (format nil "Ready to talk")))
 
 (defun aster-books ()
   "Modules for reading books I used at Cornell."
-  (mapc #'asdf:load-system
-          '( :vavasis-book :vanloan-book :tcs-chicago :rz-book
-            :norvig-book :gries-book :dennis-math-books
-            :cs611-notes)))
+  (mapc
+   #'asdf:load-system
+   '( :vavasis-book :vanloan-book :tcs-chicago :rz-book
+     :norvig-book :gries-book :dennis-math-books :cs611-notes)))
 
 (aster)
 ;(aster-books)
