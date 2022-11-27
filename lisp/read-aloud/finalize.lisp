b01cd9f2ff0922b56189ef1a2b0d76ad808ec2b7
T.V Raman <tv.raman.tv@gmail.com>
2022-11-27 10:05:47 -0800

    cleanup

--- lisp/read-aloud/finalize.lisp
+++ lisp/read-aloud/finalize.lisp
@@ -12,10 +12,5 @@
 (activate-rule 'footnote 'float)
 (activate-rule 'log 'read-base-first)
 
-;(activate-rule 'induction 'default)
-;(activate-rule 'overbrace 'default)
-;(activate-rule 'stackrel 'default)
-;(activate-rule 'underbrace 'default)
-
 (setf *follow-cross-ref-wait* 0
       *get-label-wait* 0)
