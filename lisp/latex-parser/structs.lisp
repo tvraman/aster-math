5c13168e9dc93aa2abcbce5e3bbcac9978ca1f3a
T.V Raman <tv.raman.tv@gmail.com>
2022-11-28 19:39:51 -0800

    cleanup

--- lisp/latex-parser/structs.lisp
+++ lisp/latex-parser/structs.lisp
@@ -1,6 +1,4 @@
 ;;;   -*-   Mode: LISP -*-    ;;;
- 
- 
 
 ;;; Copyright (C) 1990, 1991, 1992, 1993, 1994by T. V. Raman 
 ;;; All Rights Reserved
@@ -9,14 +7,6 @@
 
 (in-package :aster)
 
-;;; Modified: Thu Apr  2 15:36:36 EST 1992
- 
-;;; Commenting out some of the structure definitions for
-;;; which classes  are being defined.
- 
-;;; Structure definitions for various parts of a article.
- 
-
 ;;; Structure: PARSE-TABLE                                   Author: raman
 ;;; Created: Sun Jan 26 11:21:16 1992
 
@@ -24,8 +14,7 @@
 
 (defstruct (parse-table (:type list))
   (name)
-  (parser)
-  )
+  (parser))
 
 ;;; Structure: tex-macro                                    Author: raman
 ;;; Created: Wed Jan 29 10:34:35 1992
@@ -36,17 +25,13 @@
   "Holds the information about a TeX control sequence, ie. a macro"
   (name)
   (number-of-args)
-  (expand)
-  )
+  (expand))
 
 ;;; Structure: MATH-DELIMITER                                Author: raman
 ;;; Created: Tue Feb  4 18:34:12 1992
-
-;;; hold a math delimiter 
 
 (defstruct (math-delimiter (:type  list))
   (open)
   (close)
   (name)
   )
-
