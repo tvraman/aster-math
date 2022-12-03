a9ce1e1bc048d5e32ca1ec57780f98fc56389944
T.V Raman <tv.raman.tv@gmail.com>
2022-12-02 20:54:26 -0800

    ws

--- lisp/read-aloud/font-afl-state-definitions.lisp
+++ lisp/read-aloud/font-afl-state-definitions.lisp
@@ -1,7 +1,5 @@
 ;;;   -*-   Mode: LISP -*-    ;;;
  
- 
-
 ;;; Copyright (C) 1990, 1991, 1992, 1993, 1994by T. V. Raman 
 ;;; All Rights Reserved
 ;;;
@@ -13,50 +11,39 @@
 
 (define-font-rule 'emphasize
     #'(lambda(state)
-        (afl:generalized-afl-operator state
-                                      '( afl:move-to afl:pitch-range  250)
-                                      '(afl:step-by  afl:average-pitch 2)
-                                      '(afl:move-to afl:hat-rise 28)
-                                      '(afl:move-to afl:stress-rise 42)
-                                      '(afl:move-to afl:assertiveness 100)
-                                      )
-        )
-  )
+        (afl:generalized-afl-operator
+         state
+         '( afl:move-to afl:pitch-range  250)
+         '(afl:step-by  afl:average-pitch 2)
+         '(afl:move-to afl:hat-rise 28)
+         '(afl:move-to afl:stress-rise 42)
+         '(afl:move-to afl:assertiveness 100))))
 
 (define-font-rule 'sans-seriph
     #'(lambda(state)
-        (afl:generalized-afl-operator state
-                                      '( afl:move-to afl:pitch-range
-                                      250)
-                                      '(afl:move-to  afl:quickness 100
-                                      )
-                                      '(afl:step-by  afl:average-pitch 2)
-                                      '(afl:move-to afl:hat-rise 28)
-                                      '(afl:move-to afl:stress-rise 42)
-                                      '(afl:move-to afl:assertiveness 100)
-                                      )
-        )
-  )
+        (afl:generalized-afl-operator
+         state
+         '( afl:move-to afl:pitch-range 250)
+         '(afl:move-to  afl:quickness 100)
+         '(afl:step-by  afl:average-pitch 2)
+         '(afl:move-to afl:hat-rise 28)
+         '(afl:move-to afl:stress-rise 42)
+         '(afl:move-to afl:assertiveness 100))))
+
 (define-font-rule 'bold
     #'(lambda(state)
-        (afl:step-by  state
-                      'afl:smoothness
-                      2))
-  )
-
+        (afl:step-by
+         state
+         'afl:smoothness 2)))
 
 (define-font-rule 'smallcaps
     #'(lambda(state)
-        (afl:multi-step-by state
-                           '(afl:breathiness 1))
-        )
-  )
-
+        (afl:multi-step-by
+         state
+         '(afl:breathiness 1))))
 
 (define-font-rule 'large 
     #'(lambda(state)
-        (afl:step-by  state
-                      'afl:head-size
-                      2)
-        )
-  )
+        (afl:step-by
+         state
+         'afl:head-size 2)))
