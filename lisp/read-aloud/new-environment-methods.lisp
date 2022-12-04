e638ef936d0d6e66b1b7f246fdc7c737581d21c4
T.V Raman <tv.raman.tv@gmail.com>
2022-12-04 09:25:54 -0800

    cleanup

--- lisp/read-aloud/new-environment-methods.lisp
+++ lisp/read-aloud/new-environment-methods.lisp
@@ -1,6 +1,4 @@
 ;;;   -*-   Mode: LISP -*-    ;;;
- 
- 
 
 (in-package :aster)
 ;;; Separating methods on new-environments into their own file:
@@ -8,23 +6,10 @@
 (defun caption-p (self)
   (typep  self 'caption))
 
-
-
 (defmethod caption ((figure figure ))
   "Extract the caption from a figure if any. "
-  (find-if
-   #'caption-p
-   (contents figure )))
+  (find-if #'caption-p (contents figure )))
 
 (defmethod caption ((table table))
   "Extract the caption from a table  if any. "
-  (find-if
-   #'caption-p
-   (contents table )))
-
-
-
-
-
-
-
+  (find-if #'caption-p (contents table )))
